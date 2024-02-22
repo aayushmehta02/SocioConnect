67550bdf5c1be896271f68a704c0a65764ee2ff6
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-02-22 19:35:22 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const { Console } = require('console')
+const express = require('express')
+const app = express()
+app.get('/', (req,res)=> res.send("API RUNNING"))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
