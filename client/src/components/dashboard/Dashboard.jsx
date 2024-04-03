0ffb1de5e92def5083e42e977a6a7b5fcc27d003
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-04-03 14:29:22 +0530

    getCurrentProfile dashboard connection

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 function Dashboard() {
-
+  useEffect(() => {
+    async   function getCurrentProfile(){
+      try{    
+          console.log("getCurrentProfile working")
+          const res= await axios.get("http://localhost:5000/api/profile/me");
+          console.log("the res is: ", res.data)
+          secondDispatch({type: GET_PROFILE ,payload :res.data}) ;
+          console.log("full current profile working")
+      } catch(err){
+          secondDispatch({type:PROFILE_ERROR , payload:{msg: err.response.statusText, status: err.response.status}});
+          console.log("error  getting full profile", err)
+  }}},[])
  
     
   return (
